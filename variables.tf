b7ac2d7c61bc3ed1b45be90d52472a4d72988f64
np025 <130171068+np025@users.noreply.github.com>
2026-06-20 16:34:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "subscription_id" 
 {
+  type = string
   default = "1a199030-98c6-4331-b859-d17525010c4e"
 }
 variable "resource_group_name" 
 {
+  type = string
   default = "jenkins"
 }
 variable "location" 
 {
+  type = string
   default = "West US 2"
 }
