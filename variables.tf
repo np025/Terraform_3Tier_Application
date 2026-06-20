fab1319d0a65afe4001d6194f871a5dbe9464931
np025 <130171068+np025@users.noreply.github.com>
2026-06-20 16:31:19 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "subscription_id" 
+{
+  default = "1a199030-98c6-4331-b859-d17525010c4e"
+}
+variable "resource_group_name" 
+{
+  default = "jenkins"
+}
+variable "location" 
+{
+  default = "West US 2"
+}
